81978863bce0d7055dd1314db676239fb393bd56
Bahaa AL-Deen AL-Nokta <b.alnokta@aldrtech.sy>
2025-06-17 13:18:59 +0300

    separate document processing from bm25 offline service

--- src/services/offline_vectorizers/bm25.py
+++ src/services/offline_vectorizers/bm25.py
@@ -4,13 +4,11 @@ from services.processing.preprocessing import TextPreprocessor
 
 def bm25_train(docs, dataset_name):
     preprocessor = TextPreprocessor()
-    tokenized_docs = [preprocessor.preprocess_text(doc.text).split() for doc in docs]
 
     # Train the model
-    bm25 = BM25Okapi(tokenized_docs)
+    bm25 = BM25Okapi([doc.text for doc in docs], tokenizer=preprocessor.preprocess_text)
 
     # Save the model and the documents
     joblib.dump(bm25, f"data/{dataset_name}/bm25_model.joblib")
-    joblib.dump(docs, f"data/{dataset_name}/docs_list.joblib")
 
     print("✅ Model trained and saved")
