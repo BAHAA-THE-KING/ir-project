47d42de930141c92fb411c197d724472ac402dc8
Bahaa AL-Deen AL-Nokta <b.alnokta@aldrtech.sy>
2025-06-26 11:49:52 +0300

    Move bm25 evaluation to the bm25 online service

--- src/main.py
+++ src/main.py
@@ -1,61 +1,14 @@
 from config import DEFAULT_DATASET
 from loader import load_dataset_with_queries
 from services.offline_vectorizers.tfidf import tfidf_train
-from services.online_vectorizers.bm25 import bm25_search
 from services.online_vectorizers.tfidf import tfidf_search
+from services.online_vectorizers.bm25 import bm25_search
 from gui.gui import IRMainWindow
 from PyQt6.QtWidgets import QApplication
-
-import math
 
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-
-def ndcj(reli, rank):
-    return ((2** reli)-1)/math.log10(rank+1)
-
-def evaluate_bm25(dataset_name, queries, qrels):
-    NDCG = []
-
-    for i in range(len(queries)):
-        query = queries[i]
-        
-        # Search using BM25
-        results=bm25_search(dataset_name, query.text, 37, True)
-
-        # Find relevant documents for this query
-        relevant_qrels = [qrel for qrel in qrels if qrel.query_id == query.query_id]
-        relevant_qrels = sorted(relevant_qrels, key=lambda x: x.relevance, reverse=True)
-        
-        nDCG = [
-            ndcj(
-                list(
-                    filter(
-                        lambda qrel: qrel.doc_id == doc[0], relevant_qrels
-                        )
-                    )[0].relevance if list(
-                    filter(
-                        lambda qrel: qrel.doc_id == doc[0], relevant_qrels
-                        )
-                    ) else 0
-                , i+1
-            ) for i, doc in enumerate(results)]
-        
-        iDCG = [ndcj(qrel.relevance, i+1) for i, qrel in enumerate(relevant_qrels)]
-        
-        res = sum(nDCG) 
-        ires = sum(iDCG) 
-        
-        print("")
-        print(f"query: {i}")
-        print(f"query: {i}")
-        print(f"nDCG: {res}")
-        print(f"iDCG: {ires}")
-        print(f"NDCG: {res/ires*100}%")
-        NDCG.append(res/ires)
-    
-    print(f"Average NDCG: {sum(NDCG)/len(NDCG)*100}%")
     
 
 def main():
--- src/services/online_vectorizers/bm25.py
+++ src/services/online_vectorizers/bm25.py
@@ -3,6 +3,7 @@ from rank_bm25 import BM25Okapi
 from services.online_vectorizers.inverted_index import InvertedIndex
 from services.processing.preprocessing import preprocess_text
 from loader import load_dataset
+import math
 
 def bm25_search(dataset_name: str, query: str, top_k: int = 10, with_inverted_index: bool = False) -> list[tuple[int, float, str]]:
     # Load the model and the documents
@@ -46,3 +47,49 @@ def bm25_search(dataset_name: str, query: str, top_k: int = 10, with_inverted_in
             results.append((docs[idx].doc_id, scores[idx], text))
     
     return results
+
+
+def ndcj(reli, rank):
+    return ((2 ** reli) - 1) / math.log10(rank + 1)
+
+def evaluate_bm25(dataset_name, queries, qrels):
+    NDCG = []
+
+    for i in range(len(queries)):
+        query = queries[i]
+        
+        # Search using BM25
+        results=bm25_search(dataset_name, query.text, 37, True)
+
+        # Find relevant documents for this query
+        relevant_qrels = [qrel for qrel in qrels if qrel.query_id == query.query_id]
+        relevant_qrels = sorted(relevant_qrels, key=lambda x: x.relevance, reverse=True)
+        
+        nDCG = [
+            ndcj(
+                list(
+                    filter(
+                        lambda qrel: qrel.doc_id == doc[0], relevant_qrels
+                        )
+                    )[0].relevance if list(
+                    filter(
+                        lambda qrel: qrel.doc_id == doc[0], relevant_qrels
+                        )
+                    ) else 0
+                , i+1
+            ) for i, doc in enumerate(results)]
+        
+        iDCG = [ndcj(qrel.relevance, i+1) for i, qrel in enumerate(relevant_qrels)]
+        
+        res = sum(nDCG) 
+        ires = sum(iDCG) 
+        
+        print("")
+        print(f"query: {i}")
+        print(f"query: {i}")
+        print(f"nDCG: {res}")
+        print(f"iDCG: {ires}")
+        print(f"NDCG: {res/ires*100}%")
+        NDCG.append(res/ires)
+    
+    print(f"Average NDCG: {sum(NDCG)/len(NDCG)*100}%")
