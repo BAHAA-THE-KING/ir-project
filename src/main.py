90d8b3547a578b2bda65eda9166fb4eba1410001
Bahaa AL-Deen AL-Nokta <b.alnokta@aldrtech.sy>
2025-07-03 12:08:17 +0300

    Add MAP and MRR evaluation

--- src/main.py
+++ src/main.py
@@ -9,12 +9,15 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
     
 
 def main():
-    dataset_name = DEFAULT_DATASET
+    dataset_name = 'quora'
     docs, queries, qrels = load_dataset_with_queries(dataset_name)
     retriever = BM25_online()
-    retriever.evaluateNDCG(dataset_name, queries, qrels, docs)
-    
+    MRR = retriever.evaluateMRR(dataset_name, queries, qrels)
+    MAP = retriever.evaluateMAP(dataset_name, queries, qrels)
 
+    print(f"MAP={MAP}")
+    print(f"MRR={MRR}")
+    
 if __name__ == "__main__":
     main()
 
