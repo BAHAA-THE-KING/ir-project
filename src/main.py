4bde23f6eed5ecfadb587309f50cdb56c48ebf22
Bahaa AL-Deen AL-Nokta <b.alnokta@aldrtech.sy>
2025-07-08 16:44:07 +0300

    add some evaluations to main

--- src/main.py
+++ src/main.py
@@ -1,6 +1,11 @@
 from config import DEFAULT_DATASET
-from loader import load_dataset_with_queries
+from loader import load_dataset_with_queries, load_dataset
 from services.online_vectorizers.bm25 import BM25_online
+from services.offline_vectorizers.bm25 import BM25_offline
+from services.online_vectorizers.inverted_index import InvertedIndex
+from services.processing.text_preprocessor import TextPreprocessor
+import dill
+import time
 
 import sys
 import os
@@ -11,13 +16,33 @@ sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
 def main():
     dataset_name = 'quora'
     docs, queries, qrels = load_dataset_with_queries(dataset_name)
-    retriever = BM25_online()
-    MRR = retriever.evaluateMRR(dataset_name, queries, qrels)
-    MAP = retriever.evaluateMAP(dataset_name, queries, qrels)
+    print('Dataset loaded')
+    # st = time.time()
+    # BM25_offline().bm25_train(docs, dataset_name)
+    # print(f"{time.time() - st}s")
 
-    print(f"MAP={MAP}")
-    print(f"MRR={MRR}")
+    # st = time.time()
+    # index = InvertedIndex()
+    # for i, doc in enumerate([TextPreprocessor.getInstance().preprocess_text(doc.text) for doc in docs]):
+    #     index.add_document(i, doc)
+    # with open('data/antique/inverted_index.dill', 'wb') as f:
+    #     dill.dump(index, f)
+    # print(f"{time.time() - st}s")
     
+    # st = time.time()
+    # print(BM25_online().evaluateMAP(dataset_name, queries, qrels, docs))
+    # print(f"{time.time() - st}s")
+
+    # st = time.time()
+    # print(BM25_online().evaluateMRR(dataset_name, queries, qrels))
+    # print(f"{time.time() - st}s")
+
+    # st = time.time()
+    # print(BM25_online().evaluateNDCG(dataset_name, queries, qrels, docs))
+    # print(f"{time.time() - st}s")
+
+    BM25_online().search(dataset_name, "hi man, how are you ?")
+
 if __name__ == "__main__":
     main()
 
