d92182be6f81a645acf023b88294d096bafd51a9
Abood Kh <aboodkh920@gmail.com>
2025-07-12 07:51:46 +0300

    .

--- src/services/online_vectorizers/__init__.py
+++ src/services/online_vectorizers/__init__.py
@@ -10,4 +10,3 @@ from .bm25 import BM25_online
 from .tfidf import TFIDF_online
 from .embedding import Embedding_online
 from .inverted_index import InvertedIndex
-from .clustered_embedding import ClusteredEmbedding_online # Add this line
